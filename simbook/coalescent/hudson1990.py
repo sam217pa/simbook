6628b06396d32fe2e43f0521d179188cb8677a7a
molpopgen <krthornt@uci.edu>
2019-05-21 12:00:39 -0700

    Faster H1990 implementation by using tskits's foo.set_columns API

--- simbook/coalescent/hudson1990.py
+++ simbook/coalescent/hudson1990.py
@@ -19,10 +19,15 @@ def simulate(nsam: int):
 
     tc = tskit.TableCollection(1)
 
-    for i in range(nsam):
-        tc.nodes.add_row(time=0, flags=1)
-
     nodes = np.arange(2*nsam - 1, dtype=np.int32)
+    node_times = np.zeros(2*nsam - 1)
+    num_edges = 2*(2*nsam-1-nsam)
+    parent = np.zeros(num_edges, dtype=np.int32)
+    child = np.zeros(num_edges, dtype=np.int32)
+    left = np.zeros(num_edges)
+    right = np.ones(num_edges)
+    next_node_time = nsam
+    next_edge = 0
     time = 0.0
 
     n = nsam
@@ -30,7 +35,7 @@ def simulate(nsam: int):
         rcoal = (n*(n-1))/2.
         tcoal = np.random.exponential(1./rcoal)
         time += tcoal
-        tc.nodes.add_row(time=time)
+        node_times[next_node_time] = time
         ancestor = 2*nsam - n
         p = np.random.choice(n, 1)[0]
         c1 = nodes[p]
@@ -40,10 +45,16 @@ def simulate(nsam: int):
         nodes[p] = nodes[2*nsam - n]
         if c1 > c2:
             c1, c2 = c2, c1
-        tc.edges.add_row(left=0., right=1.,
-                         parent=ancestor, child=c1)
-        tc.edges.add_row(left=0., right=1.,
-                         parent=ancestor, child=c2)
+        parent[next_edge] = ancestor
+        parent[next_edge+1] = ancestor
+        child[next_edge] = c1
+        child[next_edge+1] = c2
+        next_node_time += 1
+        next_edge += 2
         n -= 1
+    tc.nodes.set_columns(time=node_times, flags=np.ones(
+        len(node_times), dtype=np.uint32))
+    tc.edges.set_columns(left=left, right=right,
+                         parent=parent, child=child)
 
     return tc.tree_sequence()
